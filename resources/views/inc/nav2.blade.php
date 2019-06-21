51aaba9f0695cd15a9b29d6cd26ab09b0c07fce1
yousefeldhan <yousefeldhan@gmail.com>
2019-06-21 12:00:27 +0200

    2

---
+++ resources/views/inc/nav2.blade.php
@@ -0,0 +1,10 @@
+<nav id="main-nav" style="height: 73px">
+                <h1><a href="index.html">ISPI</a></h1>
+                <ul style="margin-left: 10rem">
+                    <li><a href="companyViewProfile.html">View Profile</a></li>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Report Problem</a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                <a href=""><i class="far fa-question-circle"></i> Reply Recieved Questions</a>
+            </nav>
