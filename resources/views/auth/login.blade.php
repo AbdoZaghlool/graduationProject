cf3502bf7b186b6ad05122111031c95a300274b2
yousefeldhan <yousefeldhan@gmail.com>
2019-06-27 06:55:33 +0200

    may fnal

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
                 <div class="container-signin" id="container-login">
                 <div class="form-container sign-in-user-container">
 
-                   <form method="POST" action="{{ route('login') }}">
+                   <form method="POST" action="{{ url('login') }}">
                         @csrf 
 
                         <h1 style=" text-shadow: 0 0 10px rgb(22, 48, 58);color: #fff"> User Login</h1>
@@ -65,7 +65,7 @@
                     </form>
                 </div>
                 <div class="form-container sign-in-org-container">
-                    <form action="{{ url('/institute/login') }}" method="post">
+                    <form action="{{ ('/institute/login') }}" method="post">
                     @csrf
                          <h1 style=" text-shadow: 0 0 10px rgb(22, 48, 58);color: #fff
                         ">Training-Center Login</h1>
@@ -109,63 +109,7 @@
 
 
 
-                  <!--  <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>  -->
+                  
                 </div>
             </div>
         </div>
